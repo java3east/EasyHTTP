3ef4c5d1b2a84d98fbd643448284cbc7d46d6b0f
java3east <94128006+java3east@users.noreply.github.com>
2023-02-14 16:24:49 +0100

    Update Main.java

--- example/ping/Main.java
+++ example/ping/Main.java
@@ -1,7 +1,7 @@
 import org.javabeast.easy.http.*;
 import org.javabeast.easy.http.handlers.*;
 
-public static void main(String[] args) throw Exception{
+public static void main(String[] args) throws Exception{
     // create a new server on port 1997
     HTTPServer httpServer = new HTTPServer(1997);
     // add the default ping listener to the known handlers
